74121dacd6b6202ad4605a398694c3909e95f011
lsysun1@gmail.com <lsysun1@gmail.com>
2014-09-05 15:05:41 +0800

    12

---
+++ text/text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  text
+//
+//  Created by 李松玉 on 9/5/14.
+//  Copyright (c) 2014 李松玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ text/text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text
+//
+//  Created by 李松玉 on 9/5/14.
+//  Copyright (c) 2014 李松玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
